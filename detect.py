edf798d7d4bfd056bff973dce9ee8e452f4262f0
vijayalaxmiponnaboina <ponnaboinarahulyadav@gmail.com>
2026-05-26 16:14:10 +0530

    Delete detect.py

--- detect.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def detectCapitalUse(self, word: str) -> bool:
-        if word.istitle() or word.islower() or word.isupper():
-            return True
-        else:
-            return False
