5b001e4e0a1ac45e3e38ea5139fe52012206739b
vijayalaxmiponnaboina <ponnaboinarahulyadav@gmail.com>
2026-05-26 16:13:34 +0530

    Replace numberOfSpecialChars with detectCapitalUse

--- detect.py
+++ detect.py
@@ -1,8 +1,6 @@
 class Solution:
-    def numberOfSpecialChars(self, word: str) -> int:
-        s=set(word)
-        count=0
-        for ch in "abcdefghijklmnopqrstuvwxyz":
-            if ch in s and ch.upper() in s:
-                count+=1
-        return count
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.istitle() or word.islower() or word.isupper():
+            return True
+        else:
+            return False
